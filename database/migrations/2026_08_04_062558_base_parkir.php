16ee116d438d75c592eb8940a7a0793a5ee7ae05
Arfan9swn <arfan76124@gmail.com>
2026-08-04 14:44:38 +0800

    feat: Base migration TODO: Add modifiers

--- database/migrations/2026_08_04_062558_base_parkir.php
+++ database/migrations/2026_08_04_062558_base_parkir.php
@@ -33,11 +33,33 @@ return new class extends Migration
             $table->id('id_tarif');
             $table->id('id_user');
             $table->id('id_area');
-            $table->date('waktu_masuk');
-            $table->date('waktu_keluar');
+            $table->datetime('waktu_masuk');
+            $table->datetime('waktu_keluar');
             $table->int('durasi_jam');
             $table->decimal('biaya_total');
             $table->enum('status', ['masuk', 'keluar']);
+        });
+
+        Schema::create('tb_log_aktivitas', function (Blueprint $table) {
+            $table->id('id_log');
+            $table->id('id_user');
+            $table->string('aktivitas');
+            $table->datetime('waktu_aktivitas');
+        });
+
+        Schema::create('tb_tarif', function (Blueprint $table) {
+            $table->id('id_tarif');
+            $table->enum('jenis_kendaraan', ['motor', 'mobil', 'lainnya']);
+            $table->decimal('tarif_per_jam');
+        });
+
+        Schema::create('tb_user', function (Blueprint $table) {
+            $table->id('id_user');
+            $table->string('nama_lengkap');
+            $table->string('username');
+            $table->string('password');
+            $table->enum('role', ['petugas', 'admin', 'owner']);
+            $table->tinyInteger('status_aktif');
         });
     }
 
